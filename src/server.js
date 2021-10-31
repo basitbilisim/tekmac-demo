948515bea56dfd4d9a0758856c247662b452d2f1
Abdulaziz Abduazimov <abdulaziz.abduazimov.2005@gmail.com>
2021-10-31 22:25:30 +0500

    finiched

--- src/server.js
+++ src/server.js
@@ -7,6 +7,24 @@ app.use(express.json())
 app.get('/',(req,res)=> {
 	res.send(path.join(process.cwd(),'client','index.html'))
 })
+app.get('/eng',(req,res)=> {
+	res.send(path.join(process.cwd(),'client',"eng",'index.html'))
+})
+app.get('/tr',(req,res)=> {
+	res.send(path.join(process.cwd(),'client',"tr",'index.html'))
+})
+app.get('/ru',(req,res)=> {
+	res.send(path.join(process.cwd(),'client',"ru",'index.html'))
+})
+app.get('/ru/about',(req,res)=> {
+	res.send(path.join(process.cwd(),'client',"ru","about",'index.html'))
+})
+app.get('/tr/about',(req,res)=> {
+	res.send(path.join(process.cwd(),'client',"tr","about",'index.html'))
+})
+app.get('/eng/about',(req,res)=> {
+	res.send(path.join(process.cwd(),'client',"eng","about",'index.html'))
+})
 
 app.get('/about',(req,res)=>res.send(path.join(process.cwd(),'client','about','index.html')))
 
